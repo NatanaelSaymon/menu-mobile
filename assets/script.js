ba314911dce36a638982d7db64c0ef6bdb27f26b
Natanael Saymon <nsaymonsouza@gmail.com>
2020-06-21 14:55:01 -0300

    Done: melhorando a logica do menu

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,35 @@
 
-//Esse menu mobile pode ser feito das duas formas!
+//Esse menu mobile pode ser feito de tres formas!
 
-//Forma - 1 (Está é a forma que esta sendo usada nesse projeto)
-document.querySelector('#icon').addEventListener("click", function(){
-  document.getElementById('btn').classList.toggle('show') //coloca e retira classe show
+const menuMobile = document.querySelector('#icon')
+const nav = document.querySelector('#nav-list')
 
-  //Ao clicar em um link do menu, a classe .show será removida
-  var links = document.querySelectorAll('.menu-principal li a')
-  for(i=0; i < links.length; i++){
-    links[i].onclick = function(){
-      document.getElementById('btn').classList.remove('show')
-    }
-  }
+menuMobile.addEventListener("click", () =>{
+  nav.classList.toggle('show')
 })
 
-// Forma - 2 (Para ativar a forma 2, basta inserir o evento de click com o nome da função onclick="menuMobile()" na tag <i>)
+const links = document.querySelectorAll('#nav-list li a') //capturando todos os links "a"
+
+for(link of links){ //Percorrendo todos os links quando houver o click em algum link, a classe será removida.
+  link.addEventListener("click", () =>{
+    nav.classList.remove('show')
+  })
+}
+
+// Forma - 2 
+// document.querySelector('#icon').addEventListener("click", function(){
+//   document.getElementById('btn').classList.toggle('show') //coloca e retira classe show
+
+//   //Ao clicar em um link do menu, a classe .show será removida
+//   var links = document.querySelectorAll('.menu-principal li a')
+//   for(i=0; i < links.length; i++){
+//     links[i].onclick = function(){
+//       document.getElementById('btn').classList.remove('show')
+//     }
+//   }
+// })
+
+// Forma - 3 (Para ativar a forma 3, basta inserir o evento de click com o nome da função onclick="menuMobile()" na tag <i>)
 // function menuMobile(){
 //   document.getElementById('btn').classList.toggle("show")
 //   var links = document.querySelectorAll('.menu-principal li a')
